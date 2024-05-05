960313258161910ba15925e7932b7e0a0ec24f97
unknown <ayoubouahman359@gmail.com>
2024-05-05 17:40:37 +0100

    first commit

--- Closest elevator/elevator.c
+++ Closest elevator/elevator.c
@@ -2,3 +2,19 @@
 const char* elevator(int left, int right, int call) {
   return abs(call - left) < abs(call - right) ? "left" : "right";
 }
+/*
+#include <stdio.h>
+int main()
+{
+    int     left;
+    int     right;
+    int     call;
+
+    // You can change it
+    left = 0;
+    right = 0
+    call = 0;
+
+    printf("%s", elevator(left, right, call));
+    retrun (0);
+}*/
