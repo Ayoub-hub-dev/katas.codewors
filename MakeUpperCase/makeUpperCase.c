794ab7ce26621fe2aa783bf1611699f66e881a45
unknown <ayoubouahman359@gmail.com>
2024-06-10 13:23:16 +0100

    first commit

---
+++ MakeUpperCase/makeUpperCase.c
@@ -0,0 +1,10 @@
+char *makeUpperCase (char *string)
+{
+  int i = 0;
+  while (string[i] != '\0') {
+    if (string[i] >= 'a' && string[i] <= 'z')
+      string[i] = string[i] - 32;
+    i++;
+  }
+  return string;
+}
