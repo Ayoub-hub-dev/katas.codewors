ced1dcd0750650aacd9a7ae900d635810cd03a20
unknown <ayoubouahman359@gmail.com>
2024-06-25 11:21:27 +0100

    first commit

--- Calculate BMI/bmi.c
+++ Calculate BMI/bmi.c
@@ -3,7 +3,7 @@
 
 const char *bmi (int weight, double height)
 {
-  if (weight / (height * height) <= 18.5)
+  if (weight / (height * height) <= 18.5) // (weight / pow(height, 2))
     return "Underweight";
   else if ((weight / (height * height) <= 25.5))
 	  return "Normal";
