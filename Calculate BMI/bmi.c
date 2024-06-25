7d5a7124d7b608fe428258d890931bf4143a6eaa
unknown <ayoubouahman359@gmail.com>
2024-06-25 11:19:05 +0100

    first commit

---
+++ Calculate BMI/bmi.c
@@ -0,0 +1,13 @@
+/*    N.B. do not allocate memory,
+   instead: return a string literal  */
+
+const char *bmi (int weight, double height)
+{
+  if (weight / (height * height) <= 18.5)
+    return "Underweight";
+  else if ((weight / (height * height) <= 25.5))
+	  return "Normal";
+  else if ((weight / (height * height) <= 30.0))
+	  return "Overweight";
+  return "Obese";
+}
