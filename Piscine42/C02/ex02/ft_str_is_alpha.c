2702c9ae98b6abe119a4c7403146875b888939d5
unknown <ayoubouahman359@gmail.com>
2025-06-02 22:36:02 +0100

    first commit

--- Piscine42/C02/ex02/ft_str_is_alpha.c
+++ Piscine42/C02/ex02/ft_str_is_alpha.c
@@ -5,6 +5,8 @@ int ft_str_is_alpha(char *str)
 	int	i;
 
 	i = 0;
+	if (str == "")
+		return (1);
 	while (str[i] != '\0')
 	{
 		if (!((str[i] >= 65 && str[i] <= 91) || (str[i] >= 97 && str[i] <= 122)))
@@ -16,7 +18,7 @@ int ft_str_is_alpha(char *str)
 }
 
 int main(){
-	char *str = "ay88888888oub";
+	char *str = "";
 	printf("%d", ft_str_is_alpha(str));
 	return (0);
 }
