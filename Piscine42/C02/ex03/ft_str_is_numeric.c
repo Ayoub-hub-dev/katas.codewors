67b71248da66a5e021cb80889375dc252e7ec698
unknown <ayoubouahman359@gmail.com>
2025-06-03 09:39:42 +0100

    first commit

--- Piscine42/C02/ex03/ft_str_is_numeric.c
+++ Piscine42/C02/ex03/ft_str_is_numeric.c
@@ -4,6 +4,8 @@ int ft_str_is_numeric(char *str){
 	int	i;
 
 	i = 0;
+	if (str == "")
+		return (1);
 	while (str[i] != '\0')
 	{
 		if (!(str[i] >= 48 && str[i] <= 57))
@@ -15,7 +17,7 @@ int ft_str_is_numeric(char *str){
 
 int main()
 {
-	char *str = "123434";
+	char *str = "";
 	printf("%d", ft_str_is_numeric(str));
 	return (0);
 }
