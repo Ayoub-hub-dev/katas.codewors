df1f6c229bd59ebcf6afe3c45d3f679aad20fd59
unknown <ayoubouahman359@gmail.com>
2025-06-03 09:56:55 +0100

    first commit

---
+++ Piscine42/C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ft_str_is_uppercase(char *str){
+
+	if (str == "")
+		return (1);
+
+	while (*str)
+	{
+		if (!(*str >= 65 && *str <= 90))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+int main()
+{
+	char *str = "";
+	printf("%d", ft_str_is_uppercase(str));
+	return (0);
+}
