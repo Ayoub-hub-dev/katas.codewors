3f8610472f85447cbe5b12b611d9814eb34af3c6
unknown <ayoubouahman359@gmail.com>
2025-06-29 09:35:06 +0100

    first commit

--- Piscine42/C03/ex00/ft_strcmp.c
+++ Piscine42/C03/ex00/ft_strcmp.c
@@ -2,7 +2,7 @@ int ft_strcmp(char *s1, char *s2){
 
 	while(*s1 && *s2){
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return (*s1 > *s2 ? 1 : -1);
 		s1++;
 		s2++;
 	}
@@ -11,7 +11,7 @@ int ft_strcmp(char *s1, char *s2){
 #include <stdio.h>
 int main(){
 	char *s1 = "ayoub";
-	char *s2 = "ayoub";
+	char *s2 = "atoub";
 
 	printf("%d", ft_strcmp(s1, s2));
 	return (0);
